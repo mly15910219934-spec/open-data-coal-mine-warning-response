b31deadeb79c9cdbc6944b51db8b3fe34c4350c4
mly15910219934-spec <mly15910219934@gmail.com>
2026-09-23 23:39:12 +0800

    Delete tests directory

--- tests/test_dataset.py
+++
@@ -1,9 +0,0 @@
-from src.validate_data import load_dataset
-
-
-def test_dataset_acceptance_values():
-    X, y = load_dataset()
-    assert X.shape == (2584, 18)
-    assert int(y.sum()) == 170
-    assert int((y == 0).sum()) == 2414
-
